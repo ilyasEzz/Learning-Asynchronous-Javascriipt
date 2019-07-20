fb024be5c6b08b30075b01bc8c8cd24bccfdbcf0
Ilyas Ezzahid <ezzil1997@gmail.com>
2019-07-20 21:39:55 +0700

    Callback functions

---
+++ CallBacks/app.js
@@ -0,0 +1,45 @@
+const posts = [
+  { title: "Post One", body: "this is the first Post" },
+  { title: "Post Two", body: "this is the second Post" }
+];
+
+// Synchronous
+/* function createPost(post) {
+  setTimeout(() => {
+    posts.push(post);
+  }, 2000);
+}
+
+function getPosts() {
+  setTimeout(() => {
+    let output = "";
+    posts.forEach(post => {
+      output += `<li>${post.body}</li>`;
+    });
+    document.querySelector(".getOutput").innerHTML = output;
+  }, 1000);
+}
+
+createPost({ title: "Post Three", body: "this is the third Post" });
+
+getPosts(); */
+
+// Asynchronous
+function createPost(post, callback) {
+  setTimeout(() => {
+    posts.push(post);
+    callback();
+  }, 2000);
+}
+
+function getPosts() {
+  setTimeout(() => {
+    let output = "";
+    posts.forEach(post => {
+      output += `<li>${post.body}</li>`;
+    });
+    document.querySelector(".getOutput").innerHTML = output;
+  }, 1000);
+}
+
+createPost({ title: "Post Three", body: "this is the third Post" }, getPosts);
